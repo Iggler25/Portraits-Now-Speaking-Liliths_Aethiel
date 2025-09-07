f63751e0de06ef789ff408b03854e4b59afc1a8b
Iggler25 <ixgoldmichixi@gmail.com>
2025-09-08 00:28:02 +0200

    Update Stage.tsx

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -51,7 +51,7 @@ const DEFAULT_CHARACTERS: Character[] = [
   { name: "Tifa", aliases: ["Tifa Lockhart", "Lockhart"], imageUrl: "https://files.catbox.moe/654jgo.jpg" },
   { name: "Yoruichi", aliases: ["Yoruichi Shihoin", "Yoruichi Shihōin", "Shihoin", "Shihōin"], imageUrl: "https://files.catbox.moe/jj3740.jpg" },
   { name: "Jolyne", aliases: ["Jolyne Kujoh", "Jolyne", "Kujoh", "Jo"], imageUrl: "https://files.catbox.moe/qqojnd.jpg" },
-  { name: "Misa", aliases: ["Misa Amane", "Misa", "Amane", "Misa Misa"], imageUrl: "https://files.catbox.moe/o3i89a.jpg" },
+  { name: "Misa", aliases: ["Misa Amane", "Misa-Misa", "Amane", "Misa Misa"], imageUrl: "https://files.catbox.moe/o3i89a.jpg" },
 ];
 
 /** =================== Helpers =================== */
